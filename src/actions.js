3bdb48761ef650f7b887baf7b8b5541475251647
Alex Langevin <alex_langevin@hotmail.ca>
2020-10-17 17:58:46 -0400

    ex 6

---
+++ src/actions.js
@@ -0,0 +1,14 @@
+//Action-creators
+export const requestAccessToken = () => ({
+  type: 'REQUEST_ACCESS_TOKEN',
+});
+
+export const receiveAccessToken = (token) => ({
+  type: 'RECEIVE_ACCESS_TOKEN',
+  token,
+});
+
+export const receiveAccessTokenError = () => ({
+  type: "RECEIVE_ACCESS_TOKEN_ERROR",
+});
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect, } from 'react-router-dom';
 
 import GlobalStyles from "./GlobalStyles";
 import ArtistRoute from '../AristRoute/ArtistRoute';
+import {requestAccessToken, receiveAccessToken, receiveAccessTokenError} from '../../actions';
 
 const DEFAULT_ARTIST_ID = '6XYvaoDGE0VmRt83Jss9Sn';
 
 const App = () => {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(requestAccessToken());
+
+    fetch('/spotify_access_token')
+    .then((res) => res.json())
+    .then((json) => {
+      dispatch(receiveAccessToken(json.access_token));
+    })
+    .catch((err) => {
+      console.error(err);
+      dispatch(receiveAccessTokenError());
+    });
+  }, []);
+
   return (
     <>
       <GlobalStyles />
