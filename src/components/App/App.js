45db8835cd21727ec4c3c36cff102d0e2af0bffd
Alex Langevin <alex_langevin@hotmail.ca>
2020-10-17 17:16:47 -0400

    ex5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, } from 'react-router-dom';
 
 import GlobalStyles from "./GlobalStyles";
+import ArtistRoute from '../AristRoute/ArtistRoute';
+
+const DEFAULT_ARTIST_ID = '6XYvaoDGE0VmRt83Jss9Sn';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <Router>TODO</Router>
+      <Router>
+        <Switch>
+          <Route path='/artists/:id'>
+            <ArtistRoute />
+          </Route>
+          <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`}/>
+        </Switch>
+      </Router>
     </>
   );
 };
